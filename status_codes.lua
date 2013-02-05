b6409690df324255dc5cc5d25a4d42b0999d3e2d
Caleb Cauthon <calebcauthon@gmail.com>
2013-02-05 01:06:09 -0600

    changed the scope of the variables

--- status_codes.lua
+++ status_codes.lua
@@ -1,11 +1,11 @@
-local RECEIVED_INITIAL_TEXT = 'received initial text'
-local SENT_PHONE_CONFIRMATION_REQUEST = 'sent phone confirmation text'
-local RECEIVED_CONFIRMED_PHONE_NUMBER = 'received phone number'
-local SENT_PIN_REQUEST = 'sent pin request'
-local RECEIVED_PIN = 'received pin'
-local SENT_CAR_OPTIONS = 'sent car options'
+RECEIVED_INITIAL_TEXT = 'received initial text' 
+SENT_PHONE_CONFIRMATION_REQUEST = 'sent phone confirmation text'
+RECEIVED_CONFIRMED_PHONE_NUMBER = 'received phone number'
+SENT_PIN_REQUEST = 'sent pin request'
+RECEIVED_PIN = 'received pin'
+SENT_CAR_OPTIONS = 'sent car options'
 
-local set_status = function(phone_id, status)
+set_status = function(phone_id, status)
   local status_id = phone_id..'_status' 
   storage[status_id] = RECEIVED_INITIAL_TEXT
 end
@@ -15,22 +15,22 @@ get_status = function(phone_id)
   return storage[status_id]
 end
 
-local set_bluelink_pin = function(phone_id, pin) 
+set_bluelink_pin = function(phone_id, pin) 
   local bluelink_pin_id = phone_id..'_pin'
   storage[bluelink_pin_id] = pin
 end
 
-local set_bluelink_phone_number = function(phone_id, phone_number) 
+set_bluelink_phone_number = function(phone_id, phone_number) 
   local bluelink_phone_number_id = phone_id..'_bluelink_number'
   storage[bluelink_phone_number_id] = phone_number
 end
 
-local get_phone_number = function(phone_id)
+get_phone_number = function(phone_id)
   local phone_number_id = phone_id..'_number'
   return storage[phone_number_id]
 end
 
-local set_phone_number = function(phone_id, phone_number) 
+set_phone_number = function(phone_id, phone_number) 
   local phone_number_id = phone_id..'_number'
   storage[phone_number_id] = phone_number
 end
