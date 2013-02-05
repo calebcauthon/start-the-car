a591fa6414958d7ec989cfddb7faeba46560ef65
Caleb Cauthon <calebcauthon@gmail.com>
2013-02-05 00:17:34 -0600

    added status codes

---
+++ status_codes.lua
@@ -0,0 +1,6 @@
+local RECEIVED_INITIAL_TEXT = 'received initial text'
+local SENT_PHONE_CONFIRMATION_REQUEST = 'sent phone confirmation text'
+local RECEIVED_CONFIRMED_PHONE_NUMBER = 'received phone number'
+local SENT_PIN_REQUEST = 'sent pin request'
+local RECEIVED_PIN = 'received pin'
+local SENT_CAR_OPTIONS = 'sent car options'
