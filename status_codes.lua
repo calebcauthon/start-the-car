1cff28b45ef4237507a308457252a16061ed6a9f
Caleb Cauthon <calebcauthon@gmail.com>
2013-02-05 00:58:19 -0600

    added more twilio related methods

--- status_codes.lua
+++ status_codes.lua
@@ -4,3 +4,33 @@ local RECEIVED_CONFIRMED_PHONE_NUMBER = 'received phone number'
 local SENT_PIN_REQUEST = 'sent pin request'
 local RECEIVED_PIN = 'received pin'
 local SENT_CAR_OPTIONS = 'sent car options'
+
+local set_status = function(phone_id, status)
+  local status_id = phone_id..'_status' 
+  storage[status_id] = RECEIVED_INITIAL_TEXT
+end
+
+local get_status = function(phone_id) 
+  local status_id = phone_id..'_status' 
+  return storage[status_id]
+end
+
+local set_bluelink_pin = function(phone_id, pin) 
+  local bluelink_pin_id = phone_id..'_pin'
+  storage[bluelink_pin_id] = pin
+end
+
+local set_bluelink_phone_number = function(phone_id, phone_number) 
+  local bluelink_phone_number_id = phone_id..'_bluelink_number'
+  storage[bluelink_phone_number_id] = phone_number
+end
+
+local get_phone_number = function(phone_id)
+  local phone_number_id = phone_id..'_number'
+  return storage[phone_number_id]
+end
+
+local set_phone_number = function(phone_id, phone_number) 
+  local phone_number_id = phone_id..'_number'
+  storage[phone_number_id] = phone_number
+end
--- twilio_shortcuts.lua
+++ twilio_shortcuts.lua
@@ -1,6 +1,28 @@
 local ACCOUNTSID = 'ACcd4fd39d524a70f6db244dfb51adc318'
 local AUTHTOKEN = '26946965af09d519cc066753b5f89164'
+local twilio_sms_number = '+14844986324'
 
 local send_sms = function(from, to, body) 
   twilio.sms(ACCOUNTSID, AUTHTOKEN, from, to, body)
+end
+
+local send_pin_request = function(phone_id)
+  local to = get_phone_number(phone_id)
+  local body = 'What\'s your bluelink PIN?'
+  send_sms(twilio_sms_number, to, body)
+  set_status(phone_id, SENT_PIN_REQUEST)
+end
+
+local send_phone_number_request = function(phone_id)
+  local to = get_phone_number(phone_id)
+  local body = 'What\'s your bluelink phone number?'
+  send_sms(twilio_sms_number, to, body)
+  set_status(phone_id, SENT_PHONE_CONFIRMATION_REQUEST)
+end
+
+local send_car_options = function(phone_id)
+  local to = get_phone_number(phone_id)
+  local body = 'send "start engine" to start the engine'
+  send_sms(twilio_sms_number, to, body)
+  set_status(phone_id, SENT_CAR_OPTIONS)
 end
