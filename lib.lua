6e43ea90a5ad61258031ddb4908a38b285ecd0a0
Caleb Cauthon <calebcauthon@gmail.com>
2013-02-05 16:10:52 -0600

    added is_four_digits() function

--- lib.lua
+++ lib.lua
@@ -6,3 +6,12 @@ string.is_ten_digits = function(txt)
     return false
   end
 end
+
+string.is_four_digits = function(txt) 
+  txt = string.gsub(txt, '[^0-9]', '')
+  if(string.len(txt) == 4) then
+    return true
+  else
+    return false
+  end
+end
